1948e1d55da10ce41783dbb0db54ec4b3384b5ad
SiddiK-0001 <absiddik109816@gmail.com>
2024-12-04 20:54:01 +0600

    fixed forget password part

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,13 +12,15 @@ import 'react-toastify/dist/ReactToastify.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { Authcontext } from '../provider/AuthProvider';
 import { FcGoogle } from 'react-icons/fc';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
 
 
 const Login = () => {
 
     const [errorMessage, setError] = useState('')
     const [eye, setEye] = useState(false)
-    // const emailRefoo = useRef();
+    const emailRefoo = useRef();
     const navigate = useNavigate();
 
     const { loginUser,signInWithGoogle,setLoginEmail } = useContext(Authcontext)
@@ -94,14 +96,25 @@ const Login = () => {
     }
 
     const handlePass=()=>{
-        const email = document.querySelector('input[name="email"]').value;
-        setLoginEmail(email);
+        const email = emailRefoo.current.value;
+      if(email){
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            toast.success('Password reset email sent,check your email',{
+                autoClose: 500
+            });
+                               
+        })
 
-        
-        
-
-        setLoginEmail(email);
-        navigate("/forget");
+        .catch((error) => {
+          console.log(error)
+        });
+    }
+    else{        
+        toast.error("Something went wrong", {
+            autoClose: 2000,
+        });
+      }
     }
 
 
@@ -118,15 +131,16 @@ const Login = () => {
 
                     backgroundPosition: "center",
                     minHeight: "100vh",
-                    width: "100%",                   
+                    width: "100%",
+                  
                     backgroundColor: "rgba(0, 0, 0, 0.9)",
                     backgroundBlendMode: "overlay",
                 }}
             >
-                <div className='min-h-screen flex flex-col items-center justify-center' >
+                <div className='flex flex-col items-center justify-center' >
 
 
-                    <div className="hero flex items-center justify-center md:w-3/4 w-11/12 mx-auto   text-black">
+                    <div className="hero flex items-center justify-center   md:w-3/4 w-11/12 mx-auto   text-black">
                         <div className="hero-content flex-col lg:flex-row">
 
                             <div className="text-center lg:text-left">
@@ -137,13 +151,13 @@ const Login = () => {
                                 <p className='text-white text-3xl'>Don't have an Account? Please <NavLink to="/register" className='text-[#9cfe60] text-4xl underline underline-offset-4'>Register</NavLink></p>
                             </div>
 
-                            <div className="card bg-[#67ff09a3] w-full max-w-sm shrink-0 shadow-2xl">
+                            <div className="card bg-[#6ff41c81] w-full max-w-sm shrink-0 shadow-2xl">
                                 <form onSubmit={handlelogin} className="card-body">
                                     <div className="form-control">
                                         <label className="label">
                                             <span className="text-lg font-bold ">Email</span>
                                         </label>
-                                        <input type="email"  name="email" placeholder="email" className="input input-bordered" required />
+                                        <input ref={emailRefoo} type="email"  name="email" placeholder="email" className="input input-bordered" required />
                                     </div>
 
 
@@ -176,11 +190,11 @@ const Login = () => {
                                         </label>
                                     </div>
 
-                                    <div className="form-control mt-6">
-                                        <button className="btn  bg-black text-[] border-none font-bold text-xl">Login</button>
+                                    <div className="form-control mt-2">
+                                        <button className="btn  bg-black text-[#9cfe60] border-none font-bold text-xl">Login</button>
                                     </div>
 
-                                    <div className="flex items-center justify-center my-4">
+                                    <div className="flex items-center justify-center my-2">
                                         <div className="h-px bg-black flex-grow"></div>
                                         <span className="px-3 text-xl ">Or</span>
                                         <div className="h-px bg-black flex-grow"></div>
@@ -200,7 +214,9 @@ const Login = () => {
                         </div>
                     </div>
 
-                    <p className='md:text-6xl text-2xl text-white text-center font-bold '> Don't want to Login? Back to <NavLink to="/" className='text-[#9cfe60] underline underline-offset-4'>Home</NavLink></p>
+                  <div>
+                  <p className='md:text-6xl text-2xl text-white text-center font-bold '> Don't want to Login? Back to <NavLink to="/" className='text-[#9cfe60] underline underline-offset-4'>Home</NavLink></p>
+                  </div>
 
 
                 </div>
